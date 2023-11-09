c7de6c6edc3037c57c554e6bdd5d8109ba9fa288
antsilva93 <antsilva93@gmail.com>
2023-11-09 17:24:30 -0600

    task 2 complete - th: duh

--- src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
@@ -28,7 +28,7 @@ public class ListController {
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
-        tableChoices.put("all", JobData.findAll()); //added key/value pair via jobData for TODO 2
+        //tableChoices.put("all", JobData.findAll()); //added key/value pair via jobData for TODO 2 - this doesn't work
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
